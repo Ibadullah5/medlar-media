0a0b00800db473ff03c92b34a432e1332d0fe4e3
Haseeeb1 <haseebhassan701@gmail.com>
2023-07-12 02:03:34 +0500

    services complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import About from "./containers/About/About";
+import Services from "./containers/Services/Services";
 
 function App() {
-  return (
-    <>
-      {/* <About /> */}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
