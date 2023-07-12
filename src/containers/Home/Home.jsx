22c0ea0954e43c2198a6d488b4534490f5cc3735
Ibadullah <ibadullahriaz5@@example.com>
2023-07-13 00:17:30 +0500

    2 photos added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='home-maindiv'>
         <h1 className='home__heading'>MEDIA<br /> MEDLAR</h1>
         <div className='home-maindiv-secondarydiv'>
-          <h2 className='home__heading-small'>PAKISTAN</h2>
+          <h2 className='home__heading-small'>EAST PAKISTAN</h2>
           <p className='home__heading-paragraph'>
             Giving your talent a medium and a voice
             to reach the maximum potential
--- src/containers/Influencers/Influencers.jsx
+++ src/containers/Influencers/Influencers.jsx
@@ -1,6 +1,6 @@
 import './Influencers.css'
 import {
-  warisha, syed, aashir, areeba, jiya, maimoona, mustafa, nazeeha, reeja, roha
+  warisha, syed, aashir, areeba, jiya, maimoona, mustafa, nazeeha, reeja, roha, ayesha, daniya
 }
   from '../../assets'
 
@@ -41,73 +41,91 @@ const Influencers = () => {
 
           <div className="influencers__pack">
             <div className="influencers__name">
-              <p className="paragraph">JIYA KHURRAM</p>
+              <p className="paragraph">AYESHA KHALID</p>
             </div>
+            <div className="influencers__img">
+              <img src={ayesha} />
+            </div>
+          </div>
+
+          <div className="influencers__pack">
+            <div className="influencers__name">
+              <p className="paragraph">DANIYA KANWAL</p>
+            </div>
+            <div className="influencers__img">
+              <img src={daniya} />
+            </div>
+          </div>
+
+          <div className="influencers__pack">
             <div className="influencers__img">
               <img src={jiya} />
             </div>
+            <div className="influencers__name">
+              <p className="paragraph">JIYA KHURRAM</p>
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__name">
-              <p className="paragraph">MAIMOONA SHAH</p>
-            </div>
             <div className="influencers__img">
               <img src={maimoona} />
             </div>
+            <div className="influencers__name">
+              <p className="paragraph">MAIMOONA SHAH</p>
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__img">
-              <img src={mustafa} />
-            </div>
             <div className="influencers__name">
               <p className="paragraph">MUSTAFA BABER</p>
             </div>
+            <div className="influencers__img">
+              <img src={mustafa} />
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__img">
-              <img src={nazeeha} />
-            </div>
             <div className="influencers__name">
               <p className="paragraph">NAZEEHA</p>
             </div>
+            <div className="influencers__img">
+              <img src={nazeeha} />
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__name">
-              <p className="paragraph">REEJA JEELANI</p>
-            </div>
             <div className="influencers__img">
               <img src={reeja} />
             </div>
+            <div className="influencers__name">
+              <p className="paragraph">REEJA JEELANI</p>
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__name">
-              <p className="paragraph">ROHA NADEEM</p>
-            </div>
             <div className="influencers__img">
               <img src={roha} />
             </div>
+            <div className="influencers__name">
+              <p className="paragraph">ROHA NADEEM</p>
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__img">
-              <img src={syed} />
-            </div>
             <div className="influencers__name">
               <p className="paragraph">SYED ABDULA</p>
             </div>
+            <div className="influencers__img">
+              <img src={syed} />
+            </div>
           </div>
 
           <div className="influencers__pack">
-            <div className="influencers__img">
-              <img src={warisha} />
-            </div>
             <div className="influencers__name">
               <p className="paragraph">WARISHA JAVED KHAN</p>
+            </div>
+            <div className="influencers__img">
+              <img src={warisha} />
             </div>
           </div>
         </div>
